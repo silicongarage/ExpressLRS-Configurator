4b884a9be73a8739cd45e7e6d7ca71b87aee8ba6
Buster Ashmore <buster@ashmore.org>
2026-07-22 14:52:23 -0500

    fix(api): resolve type-graphql argument mapping bug causing app crash after Close
    Replace @Args decorator with individual @Arg decorators in Firmware.resolver.ts
    to fix incorrect argument mapping where gitRepository received TargetArgs fields
    instead of GitRepositoryInput fields.

--- src/api/src/graphql/resolvers/Firmware.resolver.ts
+++ src/api/src/graphql/resolvers/Firmware.resolver.ts
@@ -1,6 +1,5 @@
 import {
   Arg,
-  Args,
   Mutation,
   Query,
   Resolver,
@@ -20,6 +19,8 @@ import ClearFirmwareFilesResult from '../objects/ClearFirmwareFilesResult';
 import TargetArgs from '../args/Target';
 import Device from '../../models/Device';
 import GitRepository from '../inputs/GitRepositoryInput';
+import FirmwareSource from '../../models/enum/FirmwareSource';
+import PullRequest from '../../models/PullRequest';
 import FlashingStrategyLocatorService from '../../services/FlashingStrategyLocator';
 import { BuildProgressNotificationPayload } from '../../services/FlashingStrategyLocator/BuildProgressNotificationPayload';
 import { BuildLogUpdatePayload } from '../../services/FlashingStrategyLocator/BuildLogUpdatePayload';
@@ -35,26 +36,81 @@ export default class FirmwareResolver {
 
   @Query(() => [Device])
   async availableFirmwareTargets(
-    @Args(() => TargetArgs) args: TargetArgs,
     @Arg('gitRepository', () => GitRepository) gitRepository: GitRepository,
+    @Arg('source', () => FirmwareSource) source: FirmwareSource,
+    @Arg('gitTag', () => String) gitTag: string,
+    @Arg('gitBranch', () => String) gitBranch: string,
+    @Arg('gitCommit', () => String) gitCommit: string,
+    @Arg('localPath', () => String) localPath: string,
+    @Arg('gitPullRequest', () => PullRequest, { nullable: true }) gitPullRequest: PullRequest | null,
   ): Promise<Device[]> {
-    const strategy = await this.flashingStrategyLocatorService.locate(
-      args,
-      gitRepository,
-    );
-    return strategy.availableFirmwareTargets(args, gitRepository);
+    try {
+      if (!source) {
+        throw new Error('source is required');
+      }
+      if (!gitRepository?.url || !gitRepository?.srcFolder) {
+        throw new Error(
+          `gitRepository is invalid: url=${gitRepository?.url}, srcFolder=${gitRepository?.srcFolder}`,
+        );
+      }
+      const args = new TargetArgs();
+      args.source = source;
+      args.gitTag = gitTag;
+      args.gitBranch = gitBranch;
+      args.gitCommit = gitCommit;
+      args.localPath = localPath;
+      args.gitPullRequest = gitPullRequest;
+      const strategy = await this.flashingStrategyLocatorService.locate(
+        args,
+        gitRepository,
+      );
+      return strategy.availableFirmwareTargets(args, gitRepository);
+    } catch (e) {
+      console.error('[availableFirmwareTargets] error:', e);
+      throw e;
+    }
   }
 
   @Query(() => [UserDefine])
   async targetDeviceOptions(
-    @Args(() => TargetDeviceOptionsArgs) args: TargetDeviceOptionsArgs,
     @Arg('gitRepository', () => GitRepository) gitRepository: GitRepository,
+    @Arg('target', () => String) target: string,
+    @Arg('source', () => FirmwareSource) source: FirmwareSource,
+    @Arg('gitTag', () => String) gitTag: string,
+    @Arg('gitBranch', () => String) gitBranch: string,
+    @Arg('gitCommit', () => String) gitCommit: string,
+    @Arg('localPath', () => String) localPath: string,
+    @Arg('gitPullRequest', () => PullRequest, { nullable: true }) gitPullRequest: PullRequest | null,
   ): Promise<UserDefine[]> {
-    const strategy = await this.flashingStrategyLocatorService.locate(
-      args,
-      gitRepository,
-    );
-    return strategy.targetDeviceOptions(args, gitRepository);
+    try {
+      if (!source) {
+        throw new Error('source is required');
+      }
+      if (!target) {
+        throw new Error('target is required');
+      }
+      if (!gitRepository?.url || !gitRepository?.srcFolder) {
+        throw new Error(
+          `gitRepository is invalid: url=${gitRepository?.url}, srcFolder=${gitRepository?.srcFolder}`,
+        );
+      }
+      const args = new TargetDeviceOptionsArgs();
+      args.target = target;
+      args.source = source;
+      args.gitTag = gitTag;
+      args.gitBranch = gitBranch;
+      args.gitCommit = gitCommit;
+      args.localPath = localPath;
+      args.gitPullRequest = gitPullRequest;
+      const strategy = await this.flashingStrategyLocatorService.locate(
+        args,
+        gitRepository,
+      );
+      return strategy.targetDeviceOptions(args, gitRepository);
+    } catch (e) {
+      console.error('[targetDeviceOptions] error:', e);
+      throw e;
+    }
   }
 
   @Mutation(() => BuildFlashFirmwareResult)
